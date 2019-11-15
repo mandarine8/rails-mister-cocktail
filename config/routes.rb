a106b1c741d816aafeaf4bf477f189d7a0097e0a
Amandine Car <caramandine@gmail.com>
2019-11-15 15:37:53 -0500

    controllers and views for cocktails and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #get 'doses/new'
+  #get 'doses/create'
+  #get 'doses/destroy'
+  #get 'cocktails/index'
+  #get 'cocktails/new'
+  #get 'cocktails/create'
+  #get 'cocktails/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "rhum")
+
+mojito = Cocktail.create(name: "mojito")
+cubalibre = Cocktail.create(name: "cubalibre")
+
+dose = Dose.new(description: '6ml')
+dose.cocktail = mojito
+dose.ingredient = Ingredient.first
+dose.save
